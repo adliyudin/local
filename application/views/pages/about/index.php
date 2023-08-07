66a63ab391f1d025e7f0078a11e4d6b62c8a425c
adliyudin <aadaliyudin12@gmail.com>
2023-08-07 11:59:36 +0700

    fortify upload

---
+++ application/views/pages/about/index.php
@@ -0,0 +1,67 @@
+<!-- ============================================================== -->
+<!-- Container fluid  -->
+<!-- ============================================================== -->
+<div class="container-fluid">
+
+    <?php $this->load->view('layouts/_alert') ?>
+
+    <div class="card-group">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/reza new.jpg') ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">Reza Imami Syahbana</h4>
+                        <p class="card-text">Owner</p>
+						<p class="card-text">Jakarta</p>
+						<p class="card-text">rza.syahbana@gmail.com</p>
+                        <a href="https://api.whatsapp.com/send?phone=%20(Reza%20Imami%20Syahbana)" class="btn btn-primary">Phone Number</a>
+                    </div>
+                </div>
+            </div>
+	<div class="card-group">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/MAKEUP_2021112415565853_mh1637744328750.jpg') ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">Cinthia Aptiani</h4>
+                        <p class="card-text">Assisten Owner</p>
+						<p class="card-text">Bogor</p>
+                        <a href="https://api.whatsapp.com/send?phone=%20(Cinthia%20Aptiani)" class="btn btn-primary">Phone Number</a>
+                    </div>
+                </div>
+            </div>
+			</div>
+			</div>
+     <div class="card-group">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/reza new.jpg') ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">Reza Imami Syahbana</h4>
+                        <p class="card-text">OWNER</p>
+						<p class="card-text">Jakarta</p>
+                        <a href="https://api.whatsapp.com/send?phone=%20(Reza%20Imami%20Syahbana)" class="btn btn-primary">Phone Number</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/reza new.jpg') ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">Cinthia Aptiani</h4>
+                        <p class="card-text">OWNER</p>
+						<p class="card-text">Bogor</p>
+                        <a href="https://github.com/mramirid" class="btn btn-primary">Phone Number</a>
+                    </div>
+                </div>
+            </div>
+			
+</div>
+</div>
+</div>
+<!-- ============================================================== -->
+<!-- End Container fluid  -->
+<!-- ============================================================== -->
